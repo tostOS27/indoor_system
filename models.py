6608ab5ffb6f89608d311a18b8a743267a18894c
tostOS27 <janknyspel@gmail.com>
2025-06-05 22:18:05 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from db_config import ORMBaseModel
 from pydantic import BaseModel
 from typing import Optional
@@ -11,13 +11,23 @@ class Room(ORMBaseModel):
     room_number = Column(String, nullable=False)
     room_category_id = Column(Integer, nullable=False)
     floor_id = Column(Integer, index=True, nullable=False)
+    latitude = Column(Float, nullable=True)     # Added as float
+    longitude = Column(Float, nullable=True)    # Added as float
 
 class RoomCreate(BaseModel):
     room_number: str
     room_category_id: int
     floor_id: int
+    latitude: Optional[float] = None           # Added as float
+    longitude: Optional[float] = None          # Added as float
 
 class RoomUpdate(BaseModel):
     room_number: Optional[str] = None
     room_category_id: Optional[int] = None
     floor_id: Optional[int] = None
+    latitude: Optional[float] = None           # Added as float
+    longitude: Optional[float] = None          # Added as float
+
+class PositionUpdate(BaseModel):
+    latitude: float = None
+    longitude: float = None
